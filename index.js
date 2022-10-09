6aa9282afd2f46c9f63482dd375ca0f41c2b2afd
Restyled.io <commits@restyled.io>
2022-10-09 17:40:18 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -4,23 +4,21 @@
 var express = require("express");
 const helmet = require("helmet");
 var bodyParser = require("body-parser");
-const session = require('express-session');
+const session = require("express-session");
 var app = express();
-var languageImport = require('../Pw-manager-nodejs/language');
-var user = require('../Pw-manager-nodejs/user');
-var customer = require('../Pw-manager-nodejs/customer');
-var administration =
-    require('../Pw-manager-nodejs/administration'); // Change name!!!
+var languageImport = require("../Pw-manager-nodejs/language");
+var user = require("../Pw-manager-nodejs/user");
+var customer = require("../Pw-manager-nodejs/customer");
+var administration = require("../Pw-manager-nodejs/administration"); // Change name!!!
 var language = languageImport.getEnglish();
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs : 15 * 60 * 1000, // 15 minutes
-  max :
-      900, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders : true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders : false,  // Disable the `X-RateLimit-*` headers
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 900, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // Apply the rate limiting middleware to all requests
 app.use(limiter);
@@ -29,21 +27,21 @@ app.use(helmet());
 
 // Why do i need extended false and not true?
 // https://stackoverflow.com/questions/35931135/cannot-post-error-using-express
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use('/login', require('./login'))
 app.set("view engine", "ejs");
 // render css files
 app.use(express.static("public"));
 
-app.use(session({secret : 'secret', resave : true, saveUninitialized : true}));
+app.use(session({ secret: "secret", resave: true, saveUninitialized: true }));
 
 // routing
-app.get("/", async function(req, res) {
+app.get("/", async function (req, res) {
   console.log("redirect to / (loadData)");
 
   if (req.session.loggedIn === false) {
     console.log("user is null1 => login");
-    return res.render("login", {errormsg : ""});
+    return res.render("login", { errormsg: "" });
   }
 
   await administration.loadData(req, res);
@@ -55,37 +53,47 @@ app.get("/", async function(req, res) {
     res.send({ exists });
 }); */
 
-app.get("/index", function(req, res) { res.redirect("/"); });
+app.get("/index", function (req, res) {
+  res.redirect("/");
+});
 
-app.post(
-    "/getcustomers",
-    async function(req, res) { await administration.getCustomers(req, res); });
+app.post("/getcustomers", async function (req, res) {
+  await administration.getCustomers(req, res);
+});
 
-app.post("/addnewpw",
-         async function(req, res) { await administration.addNewPw(req, res); });
+app.post("/addnewpw", async function (req, res) {
+  await administration.addNewPw(req, res);
+});
 
-app.post("/copypw",
-         async function(req, res) { await administration.copyPw(req, res); });
+app.post("/copypw", async function (req, res) {
+  await administration.copyPw(req, res);
+});
 
-app.post("/changepw",
-         async function(req, res) { await administration.changePw(req, res); });
+app.post("/changepw", async function (req, res) {
+  await administration.changePw(req, res);
+});
 
-app.post(
-    "/changepwapp",
-    async function(req, res) { await administration.changePwApp(req, res); });
+app.post("/changepwapp", async function (req, res) {
+  await administration.changePwApp(req, res);
+});
 
-app.get("/login", function(req, res) { res.render("login", {errormsg : ""}); });
+app.get("/login", function (req, res) {
+  res.render("login", { errormsg: "" });
+});
 
-app.post("/logout",
-         async function(req, res) { await customer.logout(req, res); });
+app.post("/logout", async function (req, res) {
+  await customer.logout(req, res);
+});
 
-app.get("/logout",
-        async function(req, res) { await customer.logout(req, res); });
+app.get("/logout", async function (req, res) {
+  await customer.logout(req, res);
+});
 
-app.post("/deletepw",
-         async function(req, res) { await administration.deletePw(req, res); });
+app.post("/deletepw", async function (req, res) {
+  await administration.deletePw(req, res);
+});
 
-app.post("/signup", async function(req, res) {
+app.post("/signup", async function (req, res) {
   try {
     let status = await customer.signUp(req, res);
     user = customer.getUserFromSession(req);
@@ -94,44 +102,46 @@ app.post("/signup", async function(req, res) {
       console.log("werde user einloggen");
       await customer.signIn(req, res);
     } else {
-      return res.render("signup", {userData : user, errormsg : status});
+      return res.render("signup", { userData: user, errormsg: status });
     }
-
   } catch (err) {
     console.log("Error on Login: " + err);
-    return res.render("signup",
-                      {userData : user, errormsg : language.signUpError});
+    return res.render("signup", {
+      userData: user,
+      errormsg: language.signUpError,
+    });
   }
 });
 
-app.post("/signin",
-         async function(req, res) { await customer.signIn(req, res); });
-
-app.post("/showpw",
-         async function(req, res) { await administration.showPw(req, res); });
-
-app.get("/signup", function(req, res) {
-  user = customer.getUserFromSession(req);
-  return res.render("signup", {userData : user, errormsg : ""});
+app.post("/signin", async function (req, res) {
+  await customer.signIn(req, res);
 });
 
-app.get("/documentation", function(req, res) {
+app.post("/showpw", async function (req, res) {
+  await administration.showPw(req, res);
+});
+
+app.get("/signup", function (req, res) {
+  user = customer.getUserFromSession(req);
+  return res.render("signup", { userData: user, errormsg: "" });
+});
+
+app.get("/documentation", function (req, res) {
   user = customer.getUserFromSession(req);
   if (user.loggedIn == false) {
     customer.signIn(req, res);
   } else {
-    return res.render("documentation", {userData : user});
+    return res.render("documentation", { userData: user });
   }
 });
 
-app.get("/changepw", async function(req, res) {
+app.get("/changepw", async function (req, res) {
   user = customer.getUserFromSession(req);
-  if (user.loggedIn === false || typeof user.loggedIn === 'undefined') {
-
+  if (user.loggedIn === false || typeof user.loggedIn === "undefined") {
     res.redirect("/");
     // await customer.signIn(req, res);
   } else {
-    return res.render("changepw", {userData : user});
+    return res.render("changepw", { userData: user });
   }
 });
 
@@ -143,10 +153,12 @@ app.get("/changepw", async function(req, res) {
     return res.redirect('/');
 });*/
 
-app.post('*', function(req, res) {
+app.post("*", function (req, res) {
   console.log("redirect to / (*2)");
-  return res.redirect('/');
+  return res.redirect("/");
 });
 
 // set app to listen on port 3001
-app.listen(3001, function() { console.log("server is running on port 3001"); });
+app.listen(3001, function () {
+  console.log("server is running on port 3001");
+});
