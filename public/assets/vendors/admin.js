784164953b6a478ed3d794c060b492727f937a5c
Restyled.io <commits@restyled.io>
2022-10-11 16:09:09 +0000

    Restyled by clang-format

--- public/assets/vendors/admin.js
+++ public/assets/vendors/admin.js
@@ -1,16 +1,17 @@
-$(function () {
-    var text = $('#errorMessage').text();
-    if (text) {
-        showmessage({ content: text });
-    }
+$(function() {
+  var text = $('#errorMessage').text();
+  if (text) {
+    showmessage({content : text });
+  }
 
-    $(document).on('click', '.delete', function () {
-        var my_id_value = $(this).data('elementId');
-        $(".modal-body #elementId").val(my_id_value);
-    }),
+  $(document).on('click', '.delete',
+                 function() {
+                   var my_id_value = $(this).data('elementId');
+                   $(".modal-body #elementId").val(my_id_value);
+                 }),
 
-    $(document).on('click', ".changeapppw", function () {
+      $(document).on('click', ".changeapppw", function() {
         var my_id_value = $(this).data('changeelement');
         $(".modal-body #changeelement").val(my_id_value);
-    });
+      });
 });
