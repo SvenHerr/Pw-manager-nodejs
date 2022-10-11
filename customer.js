abd182f0e17fb59859facb2a6fbf47495bd069dc
Restyled.io <commits@restyled.io>
2022-10-11 15:53:44 +0000

    Restyled by clang-format

--- customer.js
+++ customer.js
@@ -1,164 +1,168 @@
 // This script runs on serverside
 
-//dependencies required for the app
-import connection from './Database/database.js';
-import encrypt1 from './crypto/encrypt.js';
-import User from './user.js';
+// dependencies required for the app
+import {promisify} from 'es6-promisify';
 import escape from 'lodash.escape';
-import { promisify } from 'es6-promisify';
+
+import encrypt1 from './crypto/encrypt.js';
+import connection from './Database/database.js';
+import User from './user.js';
 
 async function signUp(req) {
-    let pw = req.body.pw;
-    let pw1 = req.body.pw1;
+  let pw = req.body.pw;
+  let pw1 = req.body.pw1;
 
-    if (pw !== pw1) {
-        return 'pw missmatch';
+  if (pw !== pw1) {
+    return 'pw missmatch';
+  }
+
+  let hashedPw = encrypt1.hashPw(pw);
+  if (hashedPw === null) {
+    return 'error: Pw hash problem!';
+  }
+
+  let user = new User(null, req.body.username, req.body.firstname,
+                      req.body.lastname, hashedPw, null);
+
+  if (pw === null || pw1 === null) {
+    return 'error: Pw not found!';
+  }
+
+  if (user.username === null || user.firstname === null ||
+      user.lastname === null) {
+    return 'error: User data not found!';
+  }
+
+  try {
+    let userExists = await connection.getUserExists(user.username);
+
+    if (userExists) {
+      return 'User already exists!';
     }
 
-    let hashedPw = encrypt1.hashPw(pw);
-    if (hashedPw === null) {
-        return 'error: Pw hash problem!';
-    }
+    await connection.insertUser(user);
 
-    let user = new User(null, req.body.username, req.body.firstname, req.body.lastname, hashedPw, null);
+    req.session.pw = user.pw;
+    let save = promisify(req.session.save.bind(req.session));
+    await save();
 
-    if (pw === null || pw1 === null) {
-        return 'error: Pw not found!';
-    }
-
-    if (user.username === null || user.firstname === null || user.lastname === null) {
-        return 'error: User data not found!';
-    }
-
-    try {
-        let userExists = await connection.getUserExists(user.username);
-
-        if (userExists) {
-            return 'User already exists!';
-        }
-
-        await connection.insertUser(user);
-
-        req.session.pw = user.pw;
-        let save = promisify(req.session.save.bind(req.session));
-        await save();
-
-        return 'ok';
-    } catch (e) {
-        return e;
-    }
+    return 'ok';
+  } catch (e) {
+    return e;
+  }
 }
 
-/** signin user and store to session
+/**
+ * signin user and store to session
  *
  * @param {*} req
  * @param {*} res
  * @returns
  */
 async function signIn(req, res) {
-    if (req.session.loggedIn) {
-        res.redirect('/');
+  if (req.session.loggedIn) {
+    res.redirect('/');
+  }
+
+  try {
+    let user = await connection.getUser(req, res);
+
+    if (user === null) {
+      return res.render('login', {errormsg : req.t('loginError')});
     }
 
-    try {
-        let user = await connection.getUser(req, res);
+    setUserToSession(req, res, user);
 
-        if (user === null) {
-            return res.render('login', { errormsg: req.t('loginError') });
-        }
+    let save = promisify(req.session.save.bind(req.session));
+    await save();
 
-        setUserToSession(req, res, user);
-
-        let save = promisify(req.session.save.bind(req.session));
-        await save();
-
-        res.redirect('/');
-    } catch (err) {
-        console.log('Error on singIn: ' + err);
-    }
+    res.redirect('/');
+  } catch (err) {
+    console.log('Error on singIn: ' + err);
+  }
 }
 
-/** Logout the current user from the session
+/**
+ * Logout the current user from the session
  *
  * @param {*} req
  * @param {*} res
  * @returns
  */
 async function logout(req, res) {
-    req.session.loggedIn = false;
-    req.session.userid = 0;
-    req.session.username = '';
-    req.session.firstname = '';
-    req.session.lastname = '';
-    req.session.pw = '';
+  req.session.loggedIn = false;
+  req.session.userid = 0;
+  req.session.username = '';
+  req.session.firstname = '';
+  req.session.lastname = '';
+  req.session.pw = '';
 
-    let save = promisify(req.session.save.bind(req.session));
-    await save();
+  let save = promisify(req.session.save.bind(req.session));
+  await save();
 
-    return res.redirect('/');
+  return res.redirect('/');
 }
 
 function setUserToSession(req, res, user) {
-    let hastPw = encrypt1.hashPw(req.body.pw);
+  let hastPw = encrypt1.hashPw(req.body.pw);
 
-    if (hastPw === user.pw) {
-        req.session.loggedIn = true;
-        req.session.userid = user.id;
-        req.session.username = user.username;
-        req.session.firstname = user.firstname;
-        req.session.lastname = user.lastname;
-        req.session.pw = user.pw;
-    } else {
-        return res.render('login', { errormsg: req.t('loginError') });
-    }
+  if (hastPw === user.pw) {
+    req.session.loggedIn = true;
+    req.session.userid = user.id;
+    req.session.username = user.username;
+    req.session.firstname = user.firstname;
+    req.session.lastname = user.lastname;
+    req.session.pw = user.pw;
+  } else {
+    return res.render('login', {errormsg : req.t('loginError')});
+  }
 }
 
-/** Gets the current user from the session
+/**
+ * Gets the current user from the session
  *
  * @param {*} req
  * @param {*} res
  * @returns User object
  */
 function getUserFromSession(req) {
-    return new User(req.session.userid, req.session.username, req.session.firstname, req.session.lastname, null, req.session.loggedIn);
+  return new User(req.session.userid, req.session.username,
+                  req.session.firstname, req.session.lastname, null,
+                  req.session.loggedIn);
 }
 
-/** Change user pw(for login etc..)
+/**
+ * Change user pw(for login etc..)
  * @param {*} req
  * @param {*} res
  * @returns
  */
 async function changePw(req, res) {
-    let pwList = await connection.getAllPwFromUser(res);
+  let pwList = await connection.getAllPwFromUser(res);
 
-    if (req.session.loggedIn) {
-        if (pwList !== null) {
-            pwList.forEach(row => async function () {
-                try {
-                    await connection.updatePwDatensatz(req, row);
-                } catch (err) {
-                    console.log('ChangePW update sql: ' + err);
-                }
-            });
+  if (req.session.loggedIn) {
+    if (pwList !== null) {
+      pwList.forEach(row => async function() {
+        try {
+          await connection.updatePwDatensatz(req, row);
+        } catch (err) {
+          console.log('ChangePW update sql: ' + err);
         }
-    } else {
-        return res.render('login', { errormsg: 'Nach Pw Änderung bitte erneut anmelden' });
+      });
     }
+  } else {
+    return res.render('login',
+                      {errormsg : 'Nach Pw Änderung bitte erneut anmelden'});
+  }
 
-    req.session.pw = escape(req.body.newPw);
-    connection.updateUserPw(req);
+  req.session.pw = escape(req.body.newPw);
+  connection.updateUserPw(req);
 
-    return res.render('login', { errormsg: req.t('loginErrorPwChange') });
+  return res.render('login', {errormsg : req.t('loginErrorPwChange')});
 }
 
 export default {
-    signUp,
-    routes: {
-        post: {
-            logout,
-            signIn,
-            changePw
-        }
-    },
-    getUserFromSession
+  signUp,
+  routes: {post: {logout, signIn, changePw}},
+  getUserFromSession
 };
--- routes.js
+++ routes.js
@@ -1,90 +1,92 @@
-import user from './user.js';
-import customer from './customer.js';
 import administration from './administration.js'; // TODO: Change name !!!
+import customer from './customer.js';
+import user from './user.js';
 
-export default function (app) {
-    let routes = { customer, administration };
+export default function(app) {
+  let routes = {customer, administration};
 
-    app.get('/', async function(req, res) {
-        console.log('redirect to / (loadData)');
-    
-        if (!req.session.loggedIn) {
-            console.log('user is null1 => login');
-            return res.render('login', { errormsg: '' });
-        }
-    
-        await administration.loadData(req, res);
-    });
-    
-    app.get('/index', function(req, res) {
-        res.redirect('/');
-    });
-    
-    app.post('/signup', async function(req, res) {
-        try {
-            let status = await customer.signUp(req, res);
-            let user = customer.getUserFromSession(req);
-    
-            if (status == 'ok') {
-                console.log('werde user einloggen');
-                await customer.signIn(req, res);
-            } else {
-                return res.render('signup', { userData: user, errormsg: status });
-            }
-        } catch (err) {
-            console.log('Error on Login: ' + err);
-            return res.render('signup', { userData: user, errormsg: req.t('signup.generalError') });
-        }
-    });
-    
-    app.get('/signup', function(req, res) {
-        let user = customer.getUserFromSession(req);
-        return res.render('signup', { userData: user, errormsg: '' });
-    });
-    
-    app.get('/documentation', function(req, res) {
-        let user = customer.getUserFromSession(req);
-        if (user.loggedIn == false) {
-            customer.signIn(req, res);
-        } else {
-            return res.render('documentation', { userData: user });
-        }
-    });
-    
-    app.get('/changepw', async function(req, res) {
-        let user = customer.getUserFromSession(req);
-    
-        if (user.loggedIn === false || typeof user.loggedIn === 'undefined') {
-            res.redirect('/');
-            //await customer.signIn(req, res);
-        } else {
-            return res.render('changepw', { userData: user });
-        }
-    });
-    
-    // here you set all routes that would end in cannot get/... or cannot post/... to default page could also be you own error page
-    // ---->cant use this or it will call loadData around 10 times for no reason.
-    /*app.get('*', function(req, res) {
-        console.log("redirect to / (*1)");
-        return res.redirect('/');
-    });*/
+  app.get('/', async function(req, res) {
+    console.log('redirect to / (loadData)');
 
-    Object.keys(routes).forEach(function (path) {
-        if (routes[path]?.routes?.post) {
-            Object.keys(routes[path].routes?.post).forEach(function (method) {
-                app.post(`/${path.toLowerCase()}/${method.toLowerCase()}`, routes[path].routes?.post[method]);
-            });
-        }
+    if (!req.session.loggedIn) {
+      console.log('user is null1 => login');
+      return res.render('login', {errormsg : ''});
+    }
 
-        if (routes[path]?.routes?.get) {
-            Object.keys(routes[path].routes?.get).forEach(function (method) {
-                app.get(`/${path.toLowerCase()}/${method.toLowerCase()}`, routes[path].routes?.get[method]);
-            });
-        }
-    });
-    
-    app.post('*', function(req, res) {
-        console.log('redirect to / (*2)');
-        return res.redirect('/');
-    });
+    await administration.loadData(req, res);
+  });
+
+  app.get('/index', function(req, res) { res.redirect('/'); });
+
+  app.post('/signup', async function(req, res) {
+    try {
+      let status = await customer.signUp(req, res);
+      let user = customer.getUserFromSession(req);
+
+      if (status == 'ok') {
+        console.log('werde user einloggen');
+        await customer.signIn(req, res);
+      } else {
+        return res.render('signup', {userData : user, errormsg : status});
+      }
+    } catch (err) {
+      console.log('Error on Login: ' + err);
+      return res.render(
+          'signup', {userData : user, errormsg : req.t('signup.generalError')});
+    }
+  });
+
+  app.get('/signup', function(req, res) {
+    let user = customer.getUserFromSession(req);
+    return res.render('signup', {userData : user, errormsg : ''});
+  });
+
+  app.get('/documentation', function(req, res) {
+    let user = customer.getUserFromSession(req);
+    if (user.loggedIn == false) {
+      customer.signIn(req, res);
+    } else {
+      return res.render('documentation', {userData : user});
+    }
+  });
+
+  app.get('/changepw', async function(req, res) {
+    let user = customer.getUserFromSession(req);
+
+    if (user.loggedIn === false || typeof user.loggedIn === 'undefined') {
+      res.redirect('/');
+      // await customer.signIn(req, res);
+    } else {
+      return res.render('changepw', {userData : user});
+    }
+  });
+
+  // here you set all routes that would end in cannot get/... or cannot post/...
+  // to default page could also be you own error page
+  // ---->cant use this or it will call loadData around 10 times for no reason.
+  /*app.get('*', function(req, res) {
+      console.log("redirect to / (*1)");
+      return res.redirect('/');
+  });*/
+
+  Object.keys(routes).forEach(function(path) {
+    if (routes[path]?.routes?.post) {
+      Object.keys(routes[path].routes?.post).forEach(function(method) {
+        app.post(`/${path.toLowerCase()}/${method.toLowerCase()}`,
+                 routes[path].routes?.post[method]);
+      });
+    }
+
+    if (routes[path]?.routes?.get) {
+      Object.keys(routes[path].routes?.get).forEach(function(method) {
+        app.get(`/${path.toLowerCase()}/${method.toLowerCase()}`,
+                routes[path].routes?.get[method]);
+      });
+    }
+  });
+
+  app.post('*', function(req, res) {
+    console.log('redirect to / (*2)');
+    return res.redirect('/');
+  });
 }
