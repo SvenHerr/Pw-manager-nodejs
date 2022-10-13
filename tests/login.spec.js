d48ef9dd35139a26476d62ec424247a36820b08f
Sven Herrmann <sven92.h@web.de>
2022-10-13 15:27:29 +0200

    init selenium

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,22 @@
-describe('Test /health', () => {
-    describe('Health check on /sync', () => {
-        it('health should be okay', () => {
-            //const actualResult = healthCheckSync();
-            //expect(actualResult).to.equal('OK');
-        });
-    });
-});
+//const should = require('chai').should();
+import should1 from 'chai';
+const should = should1.should();
+
+//const {Builder, By, until} = require('selenium-webdriver');
+import {Builder, Browser, By,Key, until} from 'selenium-webdriver';
+import chromedriver from 'chromedriver';
+
+// import chromedriver so that selenium can by itself open a chrome driver
+
+(async function openChromeTest() {
+    // open chrome browser
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        // go to example website
+        await driver.get('https://example.com/');
+    } finally {
+    // close the chrome browser
+        await driver.quit();
+    }
+})();
