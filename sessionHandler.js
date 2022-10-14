f9ece068d45c25fd2529144249a6a0ec54218127
Restyled.io <commits@restyled.io>
2022-10-14 20:47:46 +0000

    Restyled by clang-format

--- sessionHandler.js
+++ sessionHandler.js
@@ -1,86 +1,90 @@
 // This script runs on serverside
 
-import { promisify } from 'es6-promisify';
+import {promisify} from 'es6-promisify';
 import escape from 'lodash.escape';
 
 /**
  * Set user data to session
- * @param {*} req 
- * @param {*} res 
- * @param {*} user 
- * @returns 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} user
+ * @returns
  */
 async function setUserToSession(req, res, user) {
-    req.session.loggedIn = true;
-    req.session.userid = user.id;
-    req.session.username = user.username;
-    req.session.firstname = user.firstname;
-    req.session.lastname = user.lastname;
-    req.session.pw = user.pw;
+  req.session.loggedIn = true;
+  req.session.userid = user.id;
+  req.session.username = user.username;
+  req.session.firstname = user.firstname;
+  req.session.lastname = user.lastname;
+  req.session.pw = user.pw;
 
-    await saveSession(req);
+  await saveSession(req);
 }
 
-/** Delte user data from session
- * 
- * @param {*} req 
+/**
+ * Delte user data from session
+ *
+ * @param {*} req
  */
 async function deleteUserFromSession(req) {
-    req.session.loggedIn = false;
-    req.session.userid = 0;
-    req.session.username = '';
-    req.session.firstname = '';
-    req.session.lastname = '';
-    req.session.pw = '';
+  req.session.loggedIn = false;
+  req.session.userid = 0;
+  req.session.username = '';
+  req.session.firstname = '';
+  req.session.lastname = '';
+  req.session.pw = '';
 
-    await saveSession(req);
+  await saveSession(req);
 }
 
-/** Regenerate session
- * 
- * @param {*} req 
+/**
+ * Regenerate session
+ *
+ * @param {*} req
  */
 async function regenerateSession(req) {
-    let regeneratedSession = promisify(req.session.regenerate.bind(req.session));
-    await regeneratedSession();
-    await saveSession(req);
+  let regeneratedSession = promisify(req.session.regenerate.bind(req.session));
+  await regeneratedSession();
+  await saveSession(req);
 }
 
-/** Update pw from session
- * 
- * @param {*} req 
+/**
+ * Update pw from session
+ *
+ * @param {*} req
  */
 async function updteUserPwFromSession(req) {
-    req.session.pw = escape(req.body.newPw);
-    await saveSession(req);
+  req.session.pw = escape(req.body.newPw);
+  await saveSession(req);
 }
 
 async function setErrormsgToSession(req, msg) {
-    req.session.errormsg = msg;
-    await saveSession(req);
+  req.session.errormsg = msg;
+  await saveSession(req);
 }
 
 async function getErrorFromSession(req) {
-    let errormsg = req.session.errormsg;
-    req.session.errormsg = '';
-    await saveSession(req);
-    return errormsg ?? '';
+  let errormsg = req.session.errormsg;
+  req.session.errormsg = '';
+  await saveSession(req);
+  return errormsg ?? '';
 }
 
-/** Save session
- * 
- * @param {*} req 
+/**
+ * Save session
+ *
+ * @param {*} req
  */
-async function saveSession(req){
-    let save = promisify(req.session.save.bind(req.session));
-    await save();
+async function saveSession(req) {
+  let save = promisify(req.session.save.bind(req.session));
+  await save();
 }
 
 export default {
-    setUserToSession,
-    deleteUserFromSession,
-    updteUserPwFromSession,
-    regenerateSession,
-    setErrormsgToSession,
-    getErrorFromSession
+  setUserToSession,
+  deleteUserFromSession,
+  updteUserPwFromSession,
+  regenerateSession,
+  setErrormsgToSession,
+  getErrorFromSession
 };
