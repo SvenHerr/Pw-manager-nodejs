1ef6a190b8750c40d543a5dfd592264d9e7eddf3
Restyled.io <commits@restyled.io>
2022-10-14 21:12:28 +0000

    Restyled by clang-format

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -1,40 +1,37 @@
 // import chromedriver so that selenium can by itself open a chrome driver
 
-import { Builder, Browser } from 'selenium-webdriver';
-import chromedriver from 'chromedriver';
 import assert from 'assert';
+import chromedriver from 'chromedriver';
+import {Browser, Builder} from 'selenium-webdriver';
+
 import helper from './helper/helper.js';
 
 // describe test
-describe('Test Login and Logout', function () {
-    // it describes expected behaviour when user perfroms search on google
-    let driver;
-    before(async function() {
-        driver = await new Builder().forBrowser(Browser.CHROME).build();
-        driver.manage().window().maximize();
-    });
+describe('Test Login and Logout', function() {
+  // it describes expected behaviour when user perfroms search on google
+  let driver;
+  before(async function() {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    driver.manage().window().maximize();
+  });
 
-    after(async function() {
-        await driver.quit();
-    });
+  after(async function() { await driver.quit(); });
 
-    it('Log in default user', async function () {
-        try {
-            let loginStatus = await helper.login(driver);
-            loginStatus.should.be.true;
-        } 
-        catch (error) {
-            console.log(error);
-        }
-    });
+  it('Log in default user', async function() {
+    try {
+      let loginStatus = await helper.login(driver);
+      loginStatus.should.be.true;
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
-    it('Logout user', async function () {
-        try {
-            let loginStatus = await helper.logout(driver);
-            loginStatus.should.be.true;
-        } 
-        catch (error) {
-            console.log(error);
-        }
-    });
+  it('Logout user', async function() {
+    try {
+      let loginStatus = await helper.logout(driver);
+      loginStatus.should.be.true;
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
